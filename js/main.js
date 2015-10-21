af37c3ee2b3386fbaf149282a6a1af2d0674e624
Leon <knights.leon@heb.com>
2015-10-21 15:31:09 -0500

    add api url

--- js/main.js
+++ js/main.js
@@ -29,8 +29,10 @@ function createRequest() {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         var response = request.responseText;
-        //console.log(response);
+        console.log(response);
     };
-    request.open('GET', 'https://www.googleapis.com/customsearch/v1?key=AIzaSyCxSXrrHLok8s4ulJo3ebeNPx3ZCkaZrIw&cx=017576662512468239146:omuauf_lfve&q=motorcycle', true);
+    //Instagram API
+    request.open('GET', 'https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e35/12142017_967266440010278_1916169648_n.jpg', true);
+    //request.open('GET', 'https://www.googleapis.com/customsearch/v1?key=AIzaSyCxSXrrHLok8s4ulJo3ebeNPx3ZCkaZrIw&cx=017576662512468239146:omuauf_lfve&q=motorcycle', true);
     request.send(null);
 }
