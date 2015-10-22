7790cf54f99f2b53906d124977b43976a0d26f15
k523163 <knights.leon@heb.com>
2015-10-22 03:48:27 -0500

    add position counter, clean up styling, update readme

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # lightbox-exercise
-HTML lightbox displaying images from a public API
+Pure HTML/CSS/JS lightbox displaying images from a public API.
 
-##Authentication:
+###Authentication:
 Imgur
 Client ID:   631ca6be53c8985
 Reset auth token - https://api.imgur.com/oauth2/authorize?client_id=631ca6be53c8985&response_type=token
--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@
  * @author Leon K.
  */
 
+//Global variables
 var images = [];
 var index = 0;
-
+/**
+ * OnLoad
+ */
 window.onload = function () {
-    //OnLoad calls
     requestApiImages();
 };
 
@@ -16,8 +18,10 @@ window.onload = function () {
  */
 function nextImage() {
     if (index < images.length - 1) {
-        console.log('NEXT - ' + (index + 1) + '/' + images.length);
-        document.getElementById('imageSlide').src = images[++index].link;
+        var i = ++index;
+        document.getElementById('imageSlide').src = images[i].link;
+        document.getElementById('sliderCount').textContent = (i + 1) + '/' + images.length;
+        document.getElementById('imageTitle').textContent = images[i].title;
     }
 }
 
@@ -26,8 +30,10 @@ function nextImage() {
  */
 function prevImage() {
     if (index > 0) {
-        console.log('PREV - ' + (index + 1) + '/' + images.length);
-        document.getElementById('imageSlide').src = images[--index].link;
+        var i = --index;
+        document.getElementById('imageSlide').src = images[i].link;
+        document.getElementById('sliderCount').textContent = (i + 1) + '/' + images.length;
+        document.getElementById('imageTitle').textContent = images[i].title;
     }
 }
 /**
@@ -35,7 +41,9 @@ function prevImage() {
  */
 function resetPosition() {
     index = 0;
-    document.getElementById('imageSlide').src = images[0].link;
+    document.getElementById('imageSlide').src = images[index].link;
+    document.getElementById('sliderCount').textContent = index + 1 + '/' + images.length;
+    document.getElementById('imageTitle').textContent = images[index].title;
 }
 
 /**
@@ -47,33 +55,35 @@ function requestApiImages() {
 }
 
 /**
- * Get all image Ids in a gallery.
+ * Get images in a gallery.
  * @param methodType
  * @param url
  */
 function createGalleryRequest(methodType, url) {
     var request = new XMLHttpRequest();
     var dataType = 'application/json';
-    var token = 'Bearer f3a41fc4887671e74479acd921ca5161c168fbc1';
+    var token = 'Bearer 8d4b08967da8d738816580691de7efc6b56f2076';
+    //Handle response
     request.onreadystatechange = function () {
         if (request.readyState === 4 && request.status === 200) {
             var response = JSON.parse(request.responseText);
-            //Save list of image IDs
+            //Save images
             for (var i = 0; i < response.data.length; i++) {
                 if (response.data[i].type === 'image/jpeg' && response.data[i].nsfw === false) {
                     images.push({title: response.data[i].title, link: response.data[i].link + '.jpg'});
                 }
             }
             document.getElementById('imageSlide').src = images[0].link;
-            console.log('IMAGE IDs:');
-            console.log(response);
-        } else if (request.status === 403) {
-            //API auth token sessions are timed
-            alert('Your API Authentication Token has expired.  Please renew.')
+            document.getElementById('sliderCount').textContent = (index + 1) + '/' + images.length;
+            document.getElementById('imageTitle').textContent = images[0].title;
         }
-        //createImageRequest(methodType, url);
+        else if (request.status === 403) {
+            //API auth tokens are timed
+            alert('Your API Authentication Token has expired.  Please renew.');
+            console.log(request);
+        }
     };
-    //Authenticated GET
+    //Authenticated GET request
     request.open(methodType, url, true);
     request.setRequestHeader('Authorization', token);
     request.setRequestHeader('Accept', dataType);
